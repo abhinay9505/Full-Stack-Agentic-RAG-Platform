ec6ef658df663b2309e46bbb7e5700d1002126ad
Rachakonda Abhinay <139874551+abhinay9505@users.noreply.github.com>
2026-06-20 19:05:27 +0530

    updated embeddings

--- backend/app/core/dependencies.py
+++ backend/app/core/dependencies.py
@@ -3,9 +3,15 @@ from langchain_groq import ChatGroq
 from langchain_community.tools import DuckDuckGoSearchRun
 from app.core.config import GROQ_API_KEY
 
-embedding_model = HuggingFaceEmbeddings(
-    model_name="all-MiniLM-L6-v2"
-)
+embedding_model = None
+
+def get_embeddings():
+    global embedding_model
+    if embedding_model is None:
+        embedding_model = HuggingFaceEmbeddings(
+            model_name="sentence-transformers/all-MiniLM-L6-v2"
+        )
+    return embedding_model
 
 llm = ChatGroq(
     model="llama-3.1-8b-instant",
